a09e014b0f2b03b4f730630396ce165b0c837445
Kezia Manlove <kezia.manlove@gmail.com>
2015-03-21 19:01:43 -0600

    stylistic revisions

--- R/UpdateLeslieFun.R
+++ R/UpdateLeslieFun.R
@@ -8,7 +8,8 @@ UpdateLeslieFun <- function(current.state, sex.ratio, samples.to.draw, tot.chain
   # 
   # current.state = character vector with label of current state
   # sex.ratio = sex ratio of lambs (usually .1)
-  # samples.to.draw = numerical range of chain steps that are outside the burn-in period in joint.posterior.coda
+  # samples.to.draw = numerical range of chain steps that are outside the burn-in period 
+  #    in joint.posterior.coda
   # tot.chains = total number of Markov chains in joint.posterior.coda
   # joint.posterior.coda = posterior coda object output from specified IPM
   # posterior.names = list of names corresponding to each vector in the coda object
@@ -43,18 +44,36 @@ UpdateLeslieFun <- function(current.state, sex.ratio, samples.to.draw, tot.chain
   post.draw$beta.adsurv.2.5 <- exp(post.draw$beta.adsurv.2.5) / (1 + exp(post.draw$beta.adsurv.2.5))
   
   if(current.state == "healthy"){
-    repros <- c(0, rep((post.draw$beta.wean.1.2 * sex.ratio), 1), rep((post.draw$beta.wean.1.3  * sex.ratio), 5), rep((post.draw$beta.wean.1.4  * sex.ratio), 6), rep((post.draw$beta.wean.1.5  * sex.ratio), 6))    
-    survs <- c(1, rep(post.draw$beta.adsurv.1.2, 1), rep(post.draw$beta.adsurv.1.3, 5), rep(post.draw$beta.adsurv.1.4, 6), rep(post.draw$beta.adsurv.1.5, 5))    
+    repros <- c(0, rep((post.draw$beta.wean.1.2 * sex.ratio), 1), 
+                rep((post.draw$beta.wean.1.3  * sex.ratio), 5), 
+                rep((post.draw$beta.wean.1.4  * sex.ratio), 6), 
+                rep((post.draw$beta.wean.1.5  * sex.ratio), 6))    
+    survs <- c(1, rep(post.draw$beta.adsurv.1.2, 1), 
+               rep(post.draw$beta.adsurv.1.3, 5), 
+               rep(post.draw$beta.adsurv.1.4, 6), 
+               rep(post.draw$beta.adsurv.1.5, 5))    
     leslie <- rbind(repros, cbind(diag(c(survs)), rep(0, dim(diag(survs))[1])))
   }
   else if(current.state == "infected"){
-    repros <- c(0, rep((post.draw$beta.wean.2.2 * sex.ratio), 1), rep((post.draw$beta.wean.2.3  * sex.ratio), 5), rep((post.draw$beta.wean.2.4  * sex.ratio), 6), rep((post.draw$beta.wean.2.5 * sex.ratio), 6))    
-    survs <- c(1, rep(post.draw$beta.adsurv.2.2, 1), rep(post.draw$beta.adsurv.2.3, 5), rep(post.draw$beta.adsurv.2.4, 6), rep(post.draw$beta.adsurv.2.5, 5))    
+    repros <- c(0, rep((post.draw$beta.wean.2.2 * sex.ratio), 1), 
+                rep((post.draw$beta.wean.2.3  * sex.ratio), 5), 
+                rep((post.draw$beta.wean.2.4  * sex.ratio), 6), 
+                rep((post.draw$beta.wean.2.5 * sex.ratio), 6))    
+    survs <- c(1, rep(post.draw$beta.adsurv.2.2, 1), 
+               rep(post.draw$beta.adsurv.2.3, 5), 
+               rep(post.draw$beta.adsurv.2.4, 6), 
+               rep(post.draw$beta.adsurv.2.5, 5))    
     leslie <- rbind(repros, cbind(diag(c(survs)), rep(0, length(survs))))
   } 
   else if(current.state == "spillover"){
-    repros <- c(0, rep((post.draw$beta.wean.2.2 * sex.ratio), 1), rep((post.draw$beta.wean.2.3  * sex.ratio), 5), rep((post.draw$beta.wean.2.4  * sex.ratio), 6), rep((post.draw$beta.wean.2.5 * sex.ratio), 6))    
-    survs <- c(1, rep(post.draw$beta.adsurv.1.2, 1), rep(post.draw$beta.adsurv.1.3, 5), rep(post.draw$beta.adsurv.1.4, 6), rep(post.draw$beta.adsurv.1.5, 5)) * (1 - intro.cost)
+    repros <- c(0, rep((post.draw$beta.wean.2.2 * sex.ratio), 1), 
+                rep((post.draw$beta.wean.2.3  * sex.ratio), 5), 
+                rep((post.draw$beta.wean.2.4  * sex.ratio), 6), 
+                rep((post.draw$beta.wean.2.5 * sex.ratio), 6))    
+    survs <- c(1, rep(post.draw$beta.adsurv.1.2, 1), 
+               rep(post.draw$beta.adsurv.1.3, 5), 
+               rep(post.draw$beta.adsurv.1.4, 6), 
+               rep(post.draw$beta.adsurv.1.5, 5)) * (1 - intro.cost)
     leslie <- rbind(repros, cbind(diag(c(survs)), rep(0, length(survs)))) 
   }
   return(leslie)
