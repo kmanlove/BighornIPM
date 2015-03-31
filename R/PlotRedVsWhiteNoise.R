38b6b79e829e9beb58f6f6f02668da9511d42026
Kezia Manlove <kezia.manlove@gmail.com>
2015-03-30 21:42:56 -0600

    Split red vs white sim and red vs white plotting

--- R/PlotRedVsWhiteNoise.R
+++ R/PlotRedVsWhiteNoise.R
@@ -1,110 +1,7 @@
-PlotRedWhiteNoise <- function(timesteps,
-                              reps,
-                              alpha.range,
-                              gamma.range,
-                              alpha.steps,
-                              gamma.steps,
-                              samples.to.use.in,
-                              pop.size, 
-                              ages.init.in, 
-                              log.y)
+PlotRedWhiteNoise <- function(RedWhiteNoiseSimsOut, 
+                              timesteps, 
+                              reps)
 {
-  ages.init <- pop.size * ages.init.in
-  intro.cost.in <- .3
-  
-  popsize.gam.2.alph.05 <- matrix(NA, ncol = timesteps, nrow = reps)
-  popsize.gam.2.alph.2 <- matrix(NA, ncol = timesteps, nrow = reps)
-  popsize.gam.2.alph1 <- matrix(NA, ncol = timesteps, nrow = reps)
- 
-  unstr.popsize.gam.2.alph.05 <- matrix(NA, ncol = timesteps, nrow = reps)
-  unstr.popsize.gam.2.alph.2 <- matrix(NA, ncol = timesteps, nrow = reps)
-  unstr.popsize.gam.2.alph1 <- matrix(NA, ncol = timesteps, nrow = reps)
- 
-  unstr.intros.popsize.gam.2.alph.05 <- matrix(NA, ncol = timesteps, nrow = reps)
-  unstr.intros.popsize.gam.2.alph.2 <- matrix(NA, ncol = timesteps, nrow = reps)
-  unstr.intros.popsize.gam.2.alph1 <- matrix(NA, ncol = timesteps, nrow = reps)
-  
-  loglambda.gam.2.alph.05 <- matrix(NA, ncol = timesteps, nrow = reps)
-  loglambda.gam.2.alph.2 <- matrix(NA, ncol = timesteps, nrow = reps)
-  loglambda.gam.2.alph1 <- matrix(NA, ncol = timesteps, nrow = reps)
-  
-  disstat.gam.2.alph.05 <- matrix(NA, ncol = timesteps, nrow = reps)
-  disstat.gam.2.alph.2 <- matrix(NA, ncol = timesteps, nrow = reps)
-  disstat.gam.2.alph1 <- matrix(NA, ncol = timesteps, nrow = reps)
-
-  disstat.intros.gam.2.alph.05 <- matrix(NA, ncol = timesteps, nrow = reps)
-  disstat.intros.gam.2.alph.2 <- matrix(NA, ncol = timesteps, nrow = reps)
-  disstat.intros.gam.2.alph1 <- matrix(NA, ncol = timesteps, nrow = reps)
-  
-  for(i in 1:reps){
-    project.gam.2.alph.05 <- ProjectFun(johnson = F, timesteps = timesteps, intro.cost = intro.cost.in, sex.ratio = .5, ages.init = ages.init, alpha = .05, gamma = .05, samples.to.draw = samples.to.use.in, tot.chains = 3, joint.posterior.coda = ipm11.coda, posterior.names = posterior.names, fixed.start.time = T)
-    project.gam.2.alph.2 <- ProjectFun(johnson = F, timesteps = timesteps, intro.cost = intro.cost.in, sex.ratio = .5, ages.init = ages.init, alpha = .2, gamma = .05, samples.to.draw = samples.to.use.in, tot.chains = 3, joint.posterior.coda = ipm11.coda, posterior.names = posterior.names, fixed.start.time = T)
-    project.gam.2.alph1 <- ProjectFun(johnson = F, timesteps = timesteps, intro.cost = intro.cost.in, sex.ratio = .5, ages.init = ages.init, alpha = .9, gamma = .05, samples.to.draw = samples.to.use.in, tot.chains = 3, joint.posterior.coda = ipm11.coda, posterior.names = posterior.names, fixed.start.time = T)
-    
-    popsize.gam.2.alph.05[i, ] <- project.gam.2.alph.05$tot.pop.size
-    popsize.gam.2.alph.2[i, ] <- project.gam.2.alph.2$tot.pop.size
-    popsize.gam.2.alph1[i, ] <- project.gam.2.alph1$tot.pop.size
-    
-#     loglambda.gam.2.alph.05[i, ] <- project.gam.2.alph.05$log.lambda.s
-#     loglambda.gam.2.alph.2[i, ] <- project.gam.2.alph.2$log.lambda.s
-#     loglambda.gam.2.alph1[i, ] <- project.gam.2.alph1$log.lambda.s
-    
-    # extract vector of disease statuses for each sim
-    disstat.gam.2.alph.05[i, ] <- sample(project.gam.2.alph.05$disease.status, length(project.gam.2.alph.05$disease.status), replace = F)
-    disstat.gam.2.alph.2[i, ] <- sample(project.gam.2.alph.2$disease.status, length(project.gam.2.alph.2$disease.status), replace = F)
-    disstat.gam.2.alph1[i, ] <- sample(project.gam.2.alph1$disease.status, length(project.gam.2.alph1$disease.status), replace = F)
-
-    unstr.project.gam.2.alph.05 <- UnstrProjectFun(johnson = F, timesteps = timesteps, 
-                                              intro.cost = intro.cost.in, sex.ratio = .5, ages.init = ages.init, 
-                                              alpha = .05, gamma = .05, samples.to.draw = samples.to.use.in, 
-                                              tot.chains = 3, joint.posterior.coda = ipm11.coda, 
-                                              posterior.names = posterior.names, fixed.start.time = T, states = disstat.gam.2.alph.05[i, ])
-
-    unstr.project.gam.2.alph.2 <- UnstrProjectFun(johnson = F, timesteps = timesteps, 
-                                          intro.cost = intro.cost.in, sex.ratio = .5, ages.init = ages.init, 
-                                          alpha = .2, gamma = .05, samples.to.draw = samples.to.use.in, 
-                                          tot.chains = 3, joint.posterior.coda = ipm11.coda, 
-                                          posterior.names = posterior.names, fixed.start.time = T, states = disstat.gam.2.alph.2[i, ])
-
-    unstr.project.gam.2.alph1 <- UnstrProjectFun(johnson = F, timesteps = timesteps, 
-                                          intro.cost = intro.cost.in, sex.ratio = .5, ages.init = ages.init, 
-                                          alpha = .05, gamma = .05, samples.to.draw = samples.to.use.in, 
-                                          tot.chains = 3, joint.posterior.coda = ipm11.coda, 
-                                          posterior.names = posterior.names, fixed.start.time = T, states = disstat.gam.2.alph1[i, ])
-
-    unstr.popsize.gam.2.alph.05[i, ] <- unstr.project.gam.2.alph.05$tot.pop.size
-    unstr.popsize.gam.2.alph.2[i, ] <- unstr.project.gam.2.alph.2$tot.pop.size
-    unstr.popsize.gam.2.alph1[i, ] <- unstr.project.gam.2.alph1$tot.pop.size
-    
-    disstat.intros.gam.2.alph.05[i, ] <- ifelse(sample(project.gam.2.alph.05$disease.status, length(project.gam.2.alph.05$disease.status), replace = F) == "spillover", "spillover", "healthy")
-    disstat.intros.gam.2.alph.2[i, ] <- ifelse(sample(project.gam.2.alph.2$disease.status, length(project.gam.2.alph.2$disease.status), replace = F) == "spillover", "spillover", "healthy")
-    disstat.intros.gam.2.alph1[i, ] <- ifelse(sample(project.gam.2.alph1$disease.status, length(project.gam.2.alph1$disease.status), replace = F) == "spillover", "spillover", "healthy")
-    
-    unstr.intros.project.gam.2.alph.05 <- UnstrProjectFun(johnson = F, timesteps = timesteps, 
-                                                   intro.cost = intro.cost.in, sex.ratio = .5, ages.init = ages.init, 
-                                                   alpha = .05, gamma = .05, samples.to.draw = samples.to.use.in, 
-                                                   tot.chains = 3, joint.posterior.coda = ipm11.coda, 
-                                                   posterior.names = posterior.names, fixed.start.time = T, states = disstat.intros.gam.2.alph.05[i, ])
-    
-    unstr.intros.project.gam.2.alph.2 <- UnstrProjectFun(johnson = F, timesteps = timesteps, 
-                                                  intro.cost = intro.cost.in, sex.ratio = .5, ages.init = ages.init, 
-                                                  alpha = .2, gamma = .05, samples.to.draw = samples.to.use.in, 
-                                                  tot.chains = 3, joint.posterior.coda = ipm11.coda, 
-                                                  posterior.names = posterior.names, fixed.start.time = T, states = disstat.intros.gam.2.alph.2[i, ])
-    
-    unstr.intros.project.gam.2.alph1 <- UnstrProjectFun(johnson = F, timesteps = timesteps, 
-                                                 intro.cost = intro.cost.in, sex.ratio = .5, ages.init = ages.init, 
-                                                 alpha = .05, gamma = .05, samples.to.draw = samples.to.use.in, 
-                                                 tot.chains = 3, joint.posterior.coda = ipm11.coda, 
-                                                 posterior.names = posterior.names, fixed.start.time = T, states = disstat.intros.gam.2.alph1[i, ])
-    
-    unstr.intros.popsize.gam.2.alph.05[i, ] <- unstr.intros.project.gam.2.alph.05$tot.pop.size
-    unstr.intros.popsize.gam.2.alph.2[i, ] <- unstr.intros.project.gam.2.alph.2$tot.pop.size
-    unstr.intros.popsize.gam.2.alph1[i, ] <- unstr.intros.project.gam.2.alph1$tot.pop.size
-    
-    print(i)
-  }  
-
 # extract 2.5th and 97.5th quantiles --#
 gam.2.alph.05.quants <- which(popsize.gam.2.alph.05[, timesteps - 1] %in% as.numeric(quantile(popsize.gam.2.alph.05[, timesteps - 1], c(0.025, 0.25, 0.5, 0.75, 0.975), type = 3, na.rm = T)))
 gam.2.alph.2.quants <- which(popsize.gam.2.alph.2[, timesteps - 1] %in% as.numeric(quantile(popsize.gam.2.alph.2[, timesteps - 1], c(0.025, 0.25, 0.5, 0.75, 0.975), type = 3, na.rm = T)))
@@ -135,6 +32,8 @@ boxgrps <- rep(c(1, 2, 3), each = reps)
 endsize.alph.05 <- c(popsize.gam.2.alph.05[, timesteps - 1], unstr.popsize.gam.2.alph.05[, timesteps - 1], unstr.intros.popsize.gam.2.alph.05[, timesteps - 1])
 endsize.alph.2 <- c(popsize.gam.2.alph.2[, timesteps - 1], unstr.popsize.gam.2.alph.2[, timesteps - 1], unstr.intros.popsize.gam.2.alph.2[, timesteps - 1])
 endsize.alph1 <- c(popsize.gam.2.alph1[, timesteps - 1], unstr.popsize.gam.2.alph1[, timesteps - 1], unstr.intros.popsize.gam.2.alph1[, timesteps - 1])
+
+
 
 plot.reps <- reps
 
