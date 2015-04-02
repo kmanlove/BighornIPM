439ae2b3f0dd3e7a18d0c1bfd334738a5e340014
Kezia Manlove <kezia.manlove@gmail.com>
2015-04-01 18:24:46 -0600

    Revised InchaustiSpectralColor to follow description in Inchausti and Halley 2002

--- R/InchaustiSpectralColor.R
+++ R/InchaustiSpectralColor.R
@@ -1,4 +1,4 @@
-InchaustiSpectralColor <- function(data){
+InchaustiSpectralColor <- function(data, PopEst){
   # VARIANCE GROWTH EXPONENT
   # Adapted from Inchausti and Halley (Science 2001) supp info
   # 1. log-transform timeseries (N + 1)
@@ -13,19 +13,47 @@ InchaustiSpectralColor <- function(data){
   # 1. apply discrete fourier transform to (full) timeseries
   #   (use appropriate windowing function)
   require(stats)
-  fft.out <- fft(data$Ewes[1:(dim(data)[1] - 1)])
-  
+  if(PopEst == F){
+  log.Ewes <- log(data$Ewes[1:(dim(data)[1] - 1)] - data$NoFemRel[1:(dim(data)[1] - 1)] + data$NoFemRem[1:(dim(data)[1] - 1)] + 1)
+  ewes.diff <- diff(log.Ewes)[-1]
+  m <- floor((dim(data)[1] - 1) / 2)
+  ewes.spectrum <- spectrum(ewes.diff, method = "pgram")
+  ewes.freq <- ewes.spectrum$freq
+  ewes.density <- ewes.spectrum$spec
+#  ewes.dft <- fft(ewes.diff)
+#  ewes.dft2 <- ewes.dft[2 : (m + 1)] 
+  plot(log(ewes.density) ~ log(ewes.freq))
   # 2. note that spectrum values will generally be complex numbers
-  # 3. Square abs val of each value in spectrum
-  sqrd.abs <- abs(fft.out) ^ 2
+#  # 3. Square abs val of each value in spectrum
+#  sqrd.abs <- abs(fft.out) ^ 2
   # 4. spectral redness exponent is the slope of the least squares regression
   #    line on this plot on the log-log scale
-  plot(sqrd.abs ~ seq(1:(dim(data)[1] - 1))^2, log = "xy")
-  loglog.fit <- lm(log(sqrd.abs) ~ seq(1:(dim(data)[1] - 1))^2)
+#  years.inv <- ((1 / seq(1:(dim(data)[1] - 1))))
+#  plot(sqrd.abs ~ years.inv, log = "xy")
+  loglog.fit <- lm(log(ewes.density) ~ log(ewes.freq))
   # 5. Reddened spectrum is associated with a line of negative slope
-  redness.exp <- summary(loglog.fit)$coefficients[2, 1]
+  redness.exp <- -1 * summary(loglog.fit)$coefficients[2, 1]
   
-  return(list(fft.out = fft.out,
+  return(list(ewes.spectrum = ewes.spectrum,
               loglog.fit = loglog.fit, 
               redness.exp = redness.exp))
+  } 
+
+  else if(PopEst == T)
+    {
+    log.PopEst <- log(data$PopEst[1:dim(data)[1] - 1] + 1)
+    m <- floor((dim(data)[1] - 1) / 2)
+    popest.diff <- diff(log.PopEst)
+    popest.spectrum <- spectrum(popest.diff, method = "pgram")
+    popest.freq <- popest.spectrum$freq
+    popest.density <- popest.spectrum$spec
+    plot(log(popest.density) ~ log(popest.freq))
+    loglog.fit <- lm(log(popest.density) ~ log(popest.freq))
+    redness.exp <- -1 * summary(loglog.fit)$coefficients[2, 1]
+    
+    return(list(popest.spectrum = popest.spectrum,
+                loglog.fit = loglog.fit, 
+                redness.exp = redness.exp))
+  }
+
 }
